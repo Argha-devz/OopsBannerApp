50df4e442bbbb218668a6167bf3ce18915403b20
Argha-devz <arghabwn2006@gmail.com>
2026-03-10 14:14:02 +0530

    UC6 implemented using static helper functions for O P S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -8,20 +8,47 @@
  */
 public class OOPSBannerApp {
 
+    // Static method for O
+    public static String[] buildO() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    // Static method for P
+    public static String[] buildP() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    // Static method for S
+    public static String[] buildS() {
+        return new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        };
+    }
+
     public static void main(String[] args) {
-    String[] banner = {
 
-    String.join("    ", "*****", "*****", "******", "*****"),
-    String.join("    ", "*   *", "*   *", "*   *", "*"),
-    String.join("    ", "*   *", "*   *", "*   *", "*"),
-    String.join("    ", "*   *", "*   *", "******", "*****"),
-    String.join("    ", "*   *", "*   *", "*", "*"),
-    String.join("    ", "*   *", "*   *", "*", "*"),
-    String.join("    ", "*****", "*****", "*", "*****")
+        String[] O = buildO();
+        String[] P = buildP();
+        String[] S = buildS();
 
-};
-for (String line : banner) {
-    System.out.println(line);
-}
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + P[i] + "   " + S[i]);
+        }
     }
 }
